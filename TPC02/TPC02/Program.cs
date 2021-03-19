ccc13db3653cdb650aae5c3198ea4e7ab14365ce
PEDRO ALEXANDRE CAPÃO RAMALHO DA SILVA <a44835@alunos.isel.pt>
2021-03-19 16:54:43 +0000

    TPC02

---
+++ TPC02/TPC02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TPC02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dll = Assembly.LoadFrom(@"C:\Users\A44835\Documents\Isel\Semestre atual\AVE\github\ave-2020-21-sem2-i41d\aula03-reflection\RestSharp.dll");
+            Type[] typeArray = dll.GetTypes();
+            printInfo(typeArray);
+
+            Console.ReadLine();
+        }
+
+        static void printInfo(Type[] type)
+        {
+            MethodInfo[] methodArray;
+            foreach (Type t in type)
+            {
+                methodArray = t.GetMethods();
+                Console.WriteLine("Type: {0}", t);
+                foreach (MethodInfo idx in methodArray)
+                {
+                    Console.WriteLine("Method: {0}", idx);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
